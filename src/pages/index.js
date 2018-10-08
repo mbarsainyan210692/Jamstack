43e163c03139711cc96572c039f72f1474663bb3
barsainy <barsainy@MacBook-Pro-59.local>
2018-10-08 14:21:29 -0700

    Introducing myself

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,9 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello World</h1>
+    <p>Welcome to my Gatsby site.</p>
+    <p>-Manu Barsainyan</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
